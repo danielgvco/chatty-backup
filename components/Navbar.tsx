574b65d9f6f7ae4d51e344345db920ab2a04a6d9
Daniel Vargas <dani@Daniels-MacBook-Air.local>
2023-09-28 19:29:36 -0700

    mobile menu layout added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 import { Button } from './ui/button'
 import { Menu, LayoutDashboard } from 'lucide-react'
 import {
@@ -12,7 +13,7 @@ import {
 
 export default function Navbar() {
     return (
-        <nav className='flex h-14 shadow-md top-0 px-12 items-center justify-between'>
+        <nav className='flex h-14 shadow-md top-0 px-4 lg:px-8 items-center justify-between'>
             <div>Logo</div>
             <div className='hidden lg:flex space-x-2 items-center'>
                 <Button variant='ghost'>Home</Button>
@@ -26,12 +27,11 @@ export default function Navbar() {
                         <Button variant='outline'><Menu /></Button>
                     </DropdownMenuTrigger>
                     <DropdownMenuContent>
-                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuItem>Home</DropdownMenuItem>
+                        <DropdownMenuItem>Pricing</DropdownMenuItem>
+                        <DropdownMenuItem>Contact</DropdownMenuItem>
                         <DropdownMenuSeparator />
-                        <DropdownMenuItem>Profile</DropdownMenuItem>
-                        <DropdownMenuItem>Billing</DropdownMenuItem>
-                        <DropdownMenuItem>Team</DropdownMenuItem>
-                        <DropdownMenuItem>Subscription</DropdownMenuItem>
+                        <DropdownMenuItem><Button><LayoutDashboard className='mr-2' />Dashboard</Button></DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
             </div>
