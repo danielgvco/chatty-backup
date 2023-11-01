7f826cb9cdb9dc0e05b407b838a2d29e53c2125d
Daniel Vargas <dani@Daniels-MacBook-Air.local>
2023-11-01 10:16:01 -0700

    added ChatHeader and modifiet Chat file, also safe commit before installing other dependencies

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react'
 import { MessagesSquare, SendHorizonal } from 'lucide-react'
 import { Button } from './ui/button'
+import ChatHeader from './ChatHeader'
 import {
     Accordion,
     AccordionContent,
@@ -10,16 +11,18 @@ import {
 
 const Chat: FC = () => {
     return (
-        <Accordion type="single" collapsible className='fixed right-4 bottom-4'>
+        <Accordion type="single" collapsible className='relative bg-white z-40 shadow'>
             <AccordionItem value="item-1">
+                <AccordionTrigger className='px-6 border-b border-zinc-300'>
+                    <ChatHeader />
+                </AccordionTrigger>
                 <AccordionContent className='h-[27rem] w-[20rem] bg-white p-4'>
                     <div className='bg-gray-200 h-[22rem] rounded mb-2'></div>
                     <div className='flex justify-between'>
-                        <form><input type='text' id='message' name='write message' className='border-2 rounded h-10 w-[14rem]'/></form>
+                        <form><input type='text' id='message' name='write message' className='border-2 rounded h-10 w-[14rem]' /></form>
                         <Button><SendHorizonal /></Button>
                     </div>
                 </AccordionContent>
-                <Button asChild className='h-12'><AccordionTrigger><div className='mr-2'><MessagesSquare /></div></AccordionTrigger></Button>
             </AccordionItem>
         </Accordion>
     )
